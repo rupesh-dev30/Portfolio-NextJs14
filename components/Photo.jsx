11542b72d3327e4bc1131bb3786764a3006d46a3
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-26 12:16:23 +0530

    photo update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -22,7 +22,7 @@ export default function Photo() {
           className="w-[258px] h-[258px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/assets/rupesh1.png"
+            src="/assets/rupesh3.png"
             priority
             quality={100}
             fill
